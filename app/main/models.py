6414ae0e87ed0dd2a3bb484c1670de2e0c2c45ea
Oliver Reera <oreera2020@gmail.com>
2024-11-16 20:40:55 -0500

    Added User table to models.py

--- app/main/models.py
+++ app/main/models.py
@@ -6,6 +6,7 @@ import sqlalchemy.orm as sqlo
 class Course(db.Model):
     id : sqlo.Mapped[int] = sqlo.mapped_column(primary_key=True)
     coursenum : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(4), index=True)
+    major : 
     title : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(150))
 
     def __repr__(self):
@@ -20,4 +21,20 @@ class Course(db.Model):
 class Section(db.Model):
     id : sqlo.Mapped[int] = sqlo.mapped_column(primary_key=True)
     sectionnum : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(5))
-    
+
+class User(db.Model):
+    id : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(9))
+    username : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(50))
+    password_hash : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(100))
+    firstname : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(50))
+    lastname : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(50))
+    user_type : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(50))
+    phone_number : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(15))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'User',
+        'polymorphic_on': user_type
+    }
+
+    def __repr__(self):
+        return '<User {} - {} - {} {}>'.format(self.id, self.username, self.firstname, self.lastname)
