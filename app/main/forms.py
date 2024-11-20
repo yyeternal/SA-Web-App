2155ce419d2bdb830b32461af7785ceb1afb1b50
ChristopherSmith101 <156460482+ChristopherSmith101@users.noreply.github.com>
2024-11-19 22:26:20 -0500

    Update forms.py
    Started a CreatePositionForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,9 +3,9 @@ from app import db
 import sqlalchemy as sqla
 
 from app.main.models import User, Instructor, Course
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, FloatField
 from wtforms_sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from wtforms.widgets import ListWidget, CheckboxInput
 
 class CourseSectionForm(FlaskForm):
@@ -16,3 +16,11 @@ class CourseSectionForm(FlaskForm):
     term = StringField('Term', validators=[DataRequired()])
     submit = SubmitField('Add')
 
+class CreatePositionForm(FlaskForm):
+    section_id = QuerySelectField('Section',
+                                 query_factory = 
+                                 get_label = )
+    open_positions = IntegerField('Number of Positions')
+    min_GPA = FloatField('Minimum GPA of Student Assistant')
+    min_grade = StringField('Minimum Grade of Student Assistant', validators=[Length(min = 0, max = 1)])
+    submit = SubmitField('Create')
