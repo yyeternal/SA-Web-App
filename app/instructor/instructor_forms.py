081fbb0a8b9a5de62974b269eb38dba4908c41b7
Jonathan_Tinti <jtintinti@gmail.com>
2024-12-11 16:35:56 -0500

    Finally fixed styling for the register page

--- app/instructor/instructor_forms.py
+++ app/instructor/instructor_forms.py
@@ -3,30 +3,33 @@ from flask_login import current_user
 from app import db
 import sqlalchemy as sqla
 
-from app.main.models import User, Instructor, Course
+from app.main.models import User, Instructor, Course, SA_Position
 from wtforms import StringField, SubmitField, PasswordField, IntegerField, FloatField, DecimalField
 from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length, NumberRange, AnyOf
 from wtforms.widgets import ListWidget, CheckboxInput
 import re
 
+def is_unique(field_name):
+    if field_name == 'sectionnum':
+        def _is_unique_sectionnum(form, field):
+            position = db.session.scalars(sqla.select(SA_Position).where(SA_Position.sectionnum == field.data)).first()
+            if position is not None:
+                # return redirect('auth.login')
+                raise ValidationError(message="There is already a section with that username")
+        return _is_unique_sectionnum
+
 def validate_phone(form, field):
     input_number = field.data
     if re.search("[a-z]", input_number) != None:
         raise ValidationError(message="Not a valid phone number")  
 
-def validate_course_num(form, field):
-    input_num = field.data
-    courses = db.session.scalars(sqla.select(Course)).all()
-    for course in courses:
-        if input_num == course.coursenum:
-            raise ValidationError(message="This section already exists!") 
 
 class CreatePositionForm(FlaskForm):
     course = QuerySelectField('Course',
                          query_factory= lambda : db.session.scalars(sqla.select(Course)),
                          get_label= lambda c : '{} - {}'.format(c.coursenum, c.title))
-    sectionnum = StringField('Section Number', validators=[DataRequired(), Length(min=1, max=10), validate_course_num])
+    sectionnum = StringField('Section Number', validators=[DataRequired(), Length(min=1, max=10), is_unique('sectionnum')])
     term = StringField('Enter the term for the class. e.g. "A 2023"', validators=[DataRequired(), Length(min=6, max=6, message='Must be in the format of A 2024')])
     open_positions = IntegerField('Number of Positions', validators=[DataRequired(), NumberRange(min=1)])
     min_GPA = DecimalField('Minimum GPA of SA', validators=[DataRequired(), NumberRange(min=0,max=4.0)])
