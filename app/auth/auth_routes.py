42ecde8d65d6b59336a93954b62552931897772a
Oliver Reera <oreera2020@gmail.com>
2024-12-11 14:53:35 -0500

    Fixed import error

--- app/auth/auth_routes.py
+++ app/auth/auth_routes.py
@@ -1,7 +1,7 @@
 
 from flask import render_template, flash, redirect, url_for
 
-from app import db, app
+from app import db
 from app.auth import auth_blueprint as bp_auth 
 import sqlalchemy as sqla
 
@@ -38,7 +38,7 @@ def login():
 
 @bp_auth.route('/response/<_external>', methods=['GET'])
 def response(_external):
-
+    pass
 
 @bp_auth.route('/user/logout', methods=['GET'])
 @login_required
