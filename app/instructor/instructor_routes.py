d0734d15e0fde8b07a11e317f118afa2f6de7db0
Oliver Reera <oreera2020@gmail.com>
2024-12-12 16:07:19 -0500

    Fixed bugs with recommended positions

--- app/instructor/instructor_routes.py
+++ app/instructor/instructor_routes.py
@@ -13,8 +13,8 @@ def view_positions():
     if not current_user.user_type == 'Instructor':
         flash('You do not have access to this page')
         return redirect(url_for('main.index'))
-    positions = SA_Position.query.filter_by(instructor_id=current_user.id).order_by(SA_Position.open_positions.desc())
-     
+    positions = SA_Position.query.filter_by(instructor_id=current_user.id).order_by(SA_Position.open_positions.desc()).all()
+
     return render_template('instructor.html', positions = positions)
 
 @bp_instructor.route('/position/create', methods=['GET', 'POST'])
